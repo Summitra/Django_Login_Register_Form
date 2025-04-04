2677988b747b10790761cd3971590ada9284cb2d
Sumitra <sumitrasamal99@gmail.com>
2025-04-04 20:01:45 +0530

    Code Edited

--- login_app/views.py
+++ login_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login,logout
+from.middlewares import auth,guest
 # Create your views here.
 
-
+@guest
 def register_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -17,6 +18,8 @@ def register_view(request):
     return render(request, 'auth/register.html' , {'form':form})
 
 
+
+@guest
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data = request.POST)
@@ -30,6 +33,8 @@ def login_view(request):
     return render(request, 'auth/login.html' , {'form':form})
 
 
+
+@auth
 def dashboard_view(request):
     return render(request, 'dashboard.html')
 
